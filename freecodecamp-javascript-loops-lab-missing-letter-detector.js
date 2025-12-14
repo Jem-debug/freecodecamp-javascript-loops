1792975f3048f7bf84bb7faaeda2809060a98f10
Jem <132315060+Jem-debug@users.noreply.github.com>
2025-12-14 10:03:29 +0800

    Implement missing letter detector function

---
+++ freecodecamp-javascript-loops-lab-missing-letter-detector.js
@@ -0,0 +1,14 @@
+const fearNotLetter = (str) => {
+  for (let i = 0; i < str.length - 1; i++) {
+    const current = str.charCodeAt(i);
+    const next = str.charCodeAt(i + 1);
+
+    if (next !== current + 1) {
+      return String.fromCharCode(current + 1);
+    }
+  }
+
+  return undefined;
+};
+
+console.log(fearNotLetter("abdf"));
